1908936ad44a79e320dfd2743071dfc5f7d72b01
Eric Billingsley <eric.billingsley.1@gmail.com>
2011-01-29 01:11:52 +0000

    Added abstract Ability class, to store ability costs and to do the effects of different abilities. For now they don't inherit from BaseObject because they don't need to update or be drawn, and I don't think they'll need to.
    A new ability folder was created where new ability classes can be defined.
    
    
    git-svn-id: file:///e/SVN/aai@39 3958679b-9987-4f48-bc3c-e9e4245d7659

---
+++ Test Game/Source/Abilities/Ability.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Test_Game
+{
+    public abstract class Ability
+    {
+        float m_cost;
+
+        public virtual void DoEffects(Vector2 position)
+        {
+        }
+
+        public float Cost
+        {
+            get { return m_cost; }
+        }
+    }
+}
--- Test Game/Source/ObjectClasses/Player.cs
+++ Test Game/Source/ObjectClasses/Player.cs
@@ -3,17 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Firecracker_Engine;
+using Microsoft.Xna.Framework;
 
 namespace Test_Game
 {
     class Player : CBaseObject
     {
         private const float CREDIT_TRICKLE_RATE = 0.25f;
-        private float m_credits; 
+        private float m_credits;
+        private Ability m_selectedAbility;
+        private List<Ability> m_abilities;
 
         public Player()
         {
             m_sObjectName = "Player";
+        }
+
+        public void DoSelectedAbility(Vector2 position) //use an ability where the user clicked
+        {
+            if (m_selectedAbility.Cost < m_credits)
+            {
+                m_credits -= m_selectedAbility.Cost;
+                m_selectedAbility.DoEffects(position);
+            }
         }
 
         public override void  Tick(float fTime)
